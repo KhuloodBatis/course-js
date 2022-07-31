fabfb87245e3de9e69c576ce34a14e682498fa83
KhuloodBatis <k.batis@bimventures.com>
2022-07-31 14:07:02 +0300

    course 32

---
+++ layouts/js/script1.js
@@ -0,0 +1,126 @@
+// let js = 'amazing';
+// // if (js === 'amazing') alert('JavaScript is FUN!')
+
+// //40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+
+// console.log(23);
+
+///++++++++++chalenge++++++++++
+// function bmi(w,h){
+//   return (w)/(h*h)
+// }
+// const markW = 78 ;
+// const markH = 1.69;
+// const johnW = 92; 
+// const johnH = 1.76; 
+
+// const markBIM1 = bmi(markW,markH)
+// console.log("mark BMI ",markBIM1);
+// const johnBIM1 = bmi(johnW,johnH)
+// console.log("john BMI",johnBIM1);
+
+// const markBIM2 = (95)/(1.88**2)
+// console.log("mark BMI 2 ",markBIM2);
+// const johnBIM2 = (85)/(1.79**2)
+// console.log("john BMI 2 ",johnBIM2);
+
+// const markHigherBmi = markBIM1>johnBIM1
+// console.log('markHigherBmi1',markHigherBmi);
+
+// let markHigherBmi2 = markBIM2>johnBIM2
+// console.log('markHigherBmi2',markHigherBmi2);
+
+//++++++++++++++++++++++++++++++++++++++++++
+
+// const age = 15 ;
+// const isOldEnough = age >= 18;
+// if(age >= 18){
+//     console.log('sarah can start driving license 🚓')
+// }else{
+//     const yearsLeft = 18-age ;
+//     console.log(`sarah is too young,waite another ${yearsLeft} years 👶`);
+// }
+///+++++++++++++++++++++++++++++++++++++++++++
+// function bmi(w,h){
+//   return (w)/(h*h)
+// }
+// const markW = 78 ;
+// const markH = 1.69;
+// const johnW = 92; 
+// const johnH = 1.76; 
+
+// const markBMI1 = bmi(markW,markH)
+// console.log("mark BMI ",markBMI1);
+// const johnBMI1 = bmi(johnW,johnH)
+// console.log("john BMI",johnBMI1);
+
+// const markBIM2 = (95)/(1.88**2)
+// console.log("mark BMI 2 ",markBIM2);
+// const johnBIM2 = (85)/(1.79**2)
+// console.log("john BMI 2 ",johnBIM2);
+
+// const markHigherBmi = markBIM1>johnBIM1
+// console.log('markHigherBmi1',markHigherBmi);
+
+// let markHigherBmi2 = markBIM2>johnBIM2
+// console.log('markHigherBmi2',markHigherBmi2);
+
+// if (markBMI1 > johnBMI1 ){
+//     console.log(`Mark bmi ${markBMI1} is higher then John ${johnBMI1} `);
+// }else{
+//     console.log(`John ${johnBMI1} is higher then Mark bmi ${markBMI1}  `);  
+// }
+
+//+++++++++++++++++++++++++++++++++++++++
+
+// const age = '18'
+// if(age === 18) console.log('you just become an adult :D (strict)');
+
+// if(age == 18) console.log('you just become an adult :D (loose)');
+
+// const favourite = prompt("what's your favourite number ?")
+//  console.log(favourite);
+//  console.log(typeof favourite);
+
+//  if ( favourite == 23 ){
+//     console.log( "Cool! 23 in amaizing ");
+//  }else if ( favourite == 7 ){
+//     console.log( "7 also is Cool! and  amaizing ");
+//  }else{
+//     console.log("is not 23 or 7 " );
+//  }
+//++++++++++++++++++++++++++++++++++++++++++++==
+// const age = 23 ;
+// // age >=18 ? console.log('I like drink pepsi 🍹'):console.log('I like drink water 🥛')
+
+// const drink = age >=18 ? ' pepsi 🍹':  'water 🥛'
+// console.log(drink);
+
+// // i can use condtion Exsp like this 
+
+// console.log(`i like drink ${age >=18 ? ' pepsi 🍹':  'water 🥛'}` );
+
+//
+//+++++++++++++++++++challenge+++++++++++++
+
+// const bill = 40;
+
+// if( bill >= 50 && bill <=300){
+//     console.log('the tip is 15%');
+// }else{
+//     console.log('the tip is 20%');
+// }
+ 
+
+// const tip = bill >= 50 && bill <=300? bil * 0.15: bill * 0.20
+
+// console.log(`the bill was ${bill} ,the tip ws ${tip} , The total value ${bill+tip}`);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+'use strict';
+
+
+
